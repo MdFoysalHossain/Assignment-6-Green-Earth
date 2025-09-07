bba51ea55ab52027be5264e485e46aa6ccf4e4c9
MdFoysalHossain <imfoysl@gmail.com>
2025-09-07 06:40:06 +0600

    Catagory Selection Added

--- js/main.js
+++ js/main.js
@@ -11,8 +11,48 @@ function loadCat(){
             for(let i = 0; i < output.categories.length; i++){
                 console.log(output.categories[i].category_name)
                 buttonContainer.innerHTML += `
-                    <button class="font-light text-[#1F2937] hover:cursor-pointer w-[200px] text-left p-1.5 rounded-md hover:bg-[#15803D] hover:text-white">${output.categories[i].category_name}</button>
+                    <button onclick="chosenCat('${output.categories[i].category_name}')" class="font-light text-[#1F2937] hover:cursor-pointer w-[200px] text-left p-1.5 rounded-md hover:bg-[#15803D] hover:text-white active:bg-[#15803D]">${output.categories[i].category_name}</button>
                 `;
+
+            }
+        })
+}
+
+
+function chosenCat(cat){
+    console.log(cat)
+    const itemsUrl = "https://openapi.programming-hero.com/api/plants";
+    let itemsContainer = document.getElementById("items-container");
+
+    fetch(itemsUrl)
+        .then(response => response.json())
+        .then(output => {
+            console.log(output.plants);
+            itemsContainer.innerHTML = "";
+
+            for(let i = 0; i < output.plants.length; i++){
+                // console.log(output.plants[i].name)
+
+                if(output.plants[i].category === cat)
+                itemsContainer.innerHTML += `
+                    <div class="card bg-base-100 w-96  pt-4 shadow-sm">
+                        <figure class="p-4 mb-2 max-h-[200px]">
+                            <img class="rounded-lg overflow-hidden"
+                            src="${output.plants[i].image}"
+                            alt="${output.plants[i].name} Image" />
+                        </figure>
+                        <div class="px-5">
+                            <h2 onclick="loadModal(${output.plants[i].id})" class="card-title text-lg">${output.plants[i].name}</h2>
+                            <span class="text-sm">${output.plants[i].description}</span>
+                            <div class="flex flex-row justify-between items-center w-full mt-4">
+                                <span class="bg-[#DCFCE7] text-[#15803D] inline-block px-3 py-1 rounded-2xl">${output.plants[i].category}</span>
+                                <p class="text-right">৳${output.plants[i].price}</p>
+                            </div>
+                            <button class="btn bg-[#15803D] rounded-3xl text-white my-3 w-full text-center py-5">Add to Cart</button>
+                        </div>
+                    </div>
+                `;
+
 
             }
         })
@@ -36,7 +76,7 @@ function loadItems(){
                         <figure class="p-4 mb-2 max-h-[200px]">
                             <img class="rounded-lg overflow-hidden"
                             src="${output.plants[i].image}"
-                            alt="Shoes" />
+                            alt="${output.plants[i].name} Image" />
                         </figure>
                         <div class="px-5">
                             <h2 onclick="loadModal(${output.plants[i].id})" class="card-title text-lg">${output.plants[i].name}</h2>
@@ -74,7 +114,7 @@ function loadModal(id){
                     modalContainer.innerHTML += `
                     <div class="modal-box">
                         <figure class="p-4 mb-2 max-h-[400px] flex justify-center items-center overflow-hidden">
-                            <img class="" src="${output.plants[i].image}" alt="Shoes" />
+                            <img class="" src="${output.plants[i].image}" alt="${output.plants[i].name}" />
                         </figure>
                         <h3 id="modal-head" class="font-bold text-2xl">${output.plants[i].name}</h3>
                         <p id="modal-cat">Catagory: ${output.plants[i].category}</p>
